f925604ee409adadc47a3f7615165d262bafef21
RVTeam <rvteamdotsquares@gmail.com>
2023-04-27 12:05:42 +0530

    change restapi live url

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,4 +2,4 @@ export {routes as Routes} from './routes';
 
 export const API_BASE_URL = "http://192.168.0.213:3000/api/v1";
 
-export const BACKEND_API_BASE_URL = "http://192.168.0.213:5000/api/v1";
+export const BACKEND_API_BASE_URL = "https://itek.onrender.com/api/v1";
